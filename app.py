20701ebaadb370effdfda41b6cf6e106bfde9ecc
olekc02 <olekcsgoods@gmail.com>
2025-08-19 14:25:19 -0500

    Bind to 0.0.0.0 and PORT for Render

--- app.py
+++ app.py
@@ -38,4 +38,7 @@ def pay():
     return render_template('pay.html', title="Pay Now")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
